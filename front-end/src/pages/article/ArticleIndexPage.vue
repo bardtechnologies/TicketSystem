257450d52b50cd71113008b9b8a89e97c35e520a
Nathan Ahrens <nathanmahrens@gmail.com>
2023-11-29 20:38:39 -0500

    Article page moves

---
+++ front-end/src/pages/article/ArticleIndexPage.vue
@@ -0,0 +1,26 @@
+<template>
+    <q-page padding>
+      <q-table 
+        v-if="rows.length>0"
+        title="Articles"
+        :rows="rows"
+      />
+    </q-page>
+  </template>
+  
+  <script setup>
+  import { ref, onMounted, defineProps } from 'vue'
+  import { api } from 'boot/axios'
+  
+//   const props = defineProps(['id'])
+  const rows = ref([])
+  
+  onMounted(() => {
+    api.get('/support-articles')
+    .then(response => {
+    rows.value = response.data
+    })
+  })
+  
+  </script>
+  
